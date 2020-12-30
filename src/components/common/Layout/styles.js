dc58b59c15aca452bf957d276ff516badc73c970
karenxiong <karen.xiong@mail.utoronto.ca>
2020-12-30 14:38:01 -0500

    Use Avenir Next as default, fallback to Nunito font

--- src/components/common/Layout/styles.js
+++ src/components/common/Layout/styles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const Global = createGlobalStyle`
   html {
-    font-family: 'Roboto', Helvetica, sans-serif;
+    font-family: 'Avenir Next', 'Nunito', 'Roboto', Helvetica, sans-serif;
     -ms-text-size-adjust: 100%;
     -webkit-text-size-adjust: 100%;
 
@@ -602,7 +602,7 @@ export const Global = createGlobalStyle`
     body {
       margin: 0;
       padding: 0;
-      font-family: 'Roboto', Helvetica, sans-serif;
+      font-family: 'Avenir Next', 'Nunito', 'Roboto', Helvetica, sans-serif;
       color: ${({ theme }) => (theme === 'light' ? 'hsla(0, 0%, 0%, 0.8)' : '#fff')};
       background-color: ${({ theme }) => (theme === 'light' ? '#fff' : '#212121')};
       transition: .3s all;
