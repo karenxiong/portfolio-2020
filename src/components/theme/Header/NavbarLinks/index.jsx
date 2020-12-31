ef4f6d9276467584d1dcb4acef529e91fa5e8474
karenxiong <karen.xiong@mail.utoronto.ca>
2020-12-30 22:24:04 -0500

    Add enlarge animation for navlink

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -9,9 +9,11 @@ const StyledLink = styled(AnchorLink)`
   color: ${({ active }) => (active ? '#333333' : '#686868')};
   font-weight: ${({ active }) => (active ? '600' : '500')};
   ${({ active }) => !active && 'font-size: 0.88rem'};
+  transition: all 0.5s ease;
 
   &:hover {
     font-weight: 600;
+    font-size: 1rem;
   }
 `;
 const NavbarLinks = ({ desktop }) => {
