8d173366ed8ad9efe4337dcda51d66e4985a33ab
karenxiong <karen.xiong@mail.utoronto.ca>
2021-05-18 18:33:22 -0400

    Update hamburger button style

--- src/components/theme/Header/Hamburger/styles.js
+++ src/components/theme/Header/Hamburger/styles.js
@@ -32,16 +32,17 @@ export const Wrapper = styled.div`
 
 export const Bar = styled.div`
 	width: 1.6rem;
-	height: .15rem;
+	height: .25rem;
 	margin-bottom: .3rem;
 	background-color: ${({ theme }) => (theme.mode === 'light' ? '#212121' : '#fff')};
 	transition: transform 500ms cubic-bezier(0.6, 0.05, 0.28, 0.91),
 	opacity 500ms,
 	box-shadow 250ms,
 	background-color 500ms;
+	border-radius: .5rem;
 
 	@media (max-width: 600px){
-		width: 1.6rem;
+		${'' /* width: 1.6rem; */}
 	}
 
 	${({ top, sidebar, theme }) =>
@@ -66,5 +67,22 @@ export const Bar = styled.div`
     `
 			background-color: ${theme.mode === 'light' ? '#212121' : '#fff'};
 			transform: translateY(-6px) rotate(-45deg);
+	`}
+
+	${({ top, theme }) =>
+    top &&
+    `
+			background-color: ${theme.color[0]};
+	`}
+	${({ mid, theme }) =>
+    mid &&
+    `
+			background-color: ${theme.color[4]};
+			width: 1.25rem;
+	`}
+	${({ bottom, theme }) =>
+    bottom &&
+    `
+			background-color: ${theme.color[8]};
 	`}
 `;
